b2aff706814ad5a2681f01265ebf59bd2aec098a
Shencanet <santi.dionis@gmail.com>
2022-10-25 18:36:04 +0200

    add: bootstrap

--- mail_index.php
+++ mail_index.php
@@ -1,47 +1,64 @@
 <!DOCTYPE html>
 <html>
-    <body>
+    <head>
+        <meta charset="utf-5" />
+        <title>Funcion mail</title>
+        <link type="text/css" rel="stylesheet" href="./css/bootstrap.min.css"/>
+        <link type="text/css" rel="stylesheet" href="./css/bootstrap-theme.css"/>
+
+
+
+    </head>
+
+<body>
     <h1>Envio mails</h1>
-        <div class="container">
+    <div class="container">
 
-            <div class="cabezera">
-                <img src="./assets/mailing1.jpg" />
-            </div>
-            <div class="cuerpo">
-                <P>Con ese formulario vamos a recoger los datos</P>
-                <br/>
-                <form action="" method="POST" enctype="multipart/form-data" class="formulario">
-                    <label for="direccion">Destinatario
-                        <input type="email" name="destino" class="form-control"/>
+        <div class="cabezera">
+            <img src="./assets/mailing1.jpg" />
+        </div>
+        <div class="cuerpo">
+            <P>Con ese formulario vamos a recoger los datos</P>
+            <br />
+            <form action="" method="POST" enctype="multipart/form-data" class="formulario">
+                <label for="direccion">Destinatario
+                    <input type="email" name="destino" class="form-control" />
 
-                    </label>
-                    <br/>
-                    <label for="asunto">Asunto
-                        <input type="text"  name="asunto" class="form-control" />
-
-
-                    </label>
-                    <br/>
-                    <label for="mensaje">Mensaje
-                        <textarea name="mensaje"></textarea>
+                </label>
+                <br />
+                <label for="asunto">Asunto
+                    <input type="text" name="asunto" class="form-control" />
 
 
-                    </label>
+                </label>
+                <br />
+                <label for="mensaje">Mensaje
+                    <textarea name="mensaje" class="form-control"></textarea>
+                    <br />
+                    <input type="submit" class="btn-success" value="Enviar" />
+                
+                
+                </label>
 
 
 
 
 
-                </form>
-
-
-            </div>
-
+            </form>
 
 
         </div>
 
 
 
-    </body>
-    </html>
+    </div>
+
+
+<footer>
+
+<hr/>
+&COPY; SHENCANET <?php echo date ('D M Y');?>
+</footer>
+</body>
+
+</html>
